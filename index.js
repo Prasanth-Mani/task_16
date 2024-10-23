586d0c97bb02e0ef94307bc79ceec0b2ce46b94e
Prasanth-Mani <prasanthportfolio1998@gmail.com>
2024-10-23 16:08:23 +0530

    Call Back

---
+++ index.js
@@ -0,0 +1,41 @@
+let countdown = 10;
+var timer = document.getElementById('sec');
+
+setTimeout(() => {
+    timer.innerText = countdown--;
+    setTimeout(() => {
+        timer.innerText = countdown--;
+        setTimeout(() => {
+            timer.innerText = countdown--;
+            setTimeout(() => {
+                timer.innerText = countdown--;
+                setTimeout(() => {
+                    timer.innerText = countdown--;
+                    setTimeout(() => {
+                        timer.innerText = countdown--;
+                        setTimeout(() => {
+                            timer.innerText = countdown--;
+                            setTimeout(() => {
+                                timer.innerText = countdown--;
+                                setTimeout(() => {
+                                    timer.innerText = countdown--;
+                                    setTimeout(() => {
+                                        timer.innerText = countdown--;
+                                        setTimeout(() => {
+                                            timer.innerText = '\'Happy Independence Day\'';
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
+
+
+
+
+
